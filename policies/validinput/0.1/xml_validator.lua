ef57e2318c63cb6327c81efa90e8071e7e967c31
Daria Mayorova <dmayorova@redhat.com>
2019-12-01 23:45:02 +0100

    Testing...

---
+++ policies/validinput/0.1/xml_validator.lua
@@ -0,0 +1,15 @@
+--- XMl Validator
+-- Validates an XML against a schema.
+
+local _M = { }
+
+--- Validate an XML object
+-- Checks if XML content is valid according to the given schema.
+-- @tparam table xml_content XML content
+-- @treturn boolean True if the XML is valid. False otherwise.
+-- @treturn string Error message only when the XML is invalid.
+function _M.validate(xml_content)
+
+end
+
+return _M
